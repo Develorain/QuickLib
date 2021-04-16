980ea20e519956be1c68ee7129564be21ef53974
lum29 <lm452498@gmail.com>
2021-04-17 04:02:20 +0800

    Add SignIn function

---
+++ front/src/Login.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import Modal from 'react-modal';
+import userIcon from './img/user.svg';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        modalIsOpen: false,
+        name: '',
+        password: '',
+        sign:'Sign In',
+        isDisabled: false
+       
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
+    
+};
+  
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  submitModal = () => {
+    if (!this.state.name.includes('@mcmaster.ca') ){
+      alert("This is not a Valid McMaster Account!");
+    }
+    else{
+      this.setState({ modalIsOpen: false });
+      this.setState({ sign: this.state.name });
+      this.setState({ isDisabled: true });
+    }
+  }
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  handleChange(e) {
+    this.setState({ name: e.target.value });
+    
+}
+
+  handleChange2(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+       
+        <input type='text' onClick={this.openModal} value={this.state.sign} disabled={this.state.isDisabled}
+        style={{marginBottom:'0px',marginTop:'0px',fontSize:'16px',marginLeft:'6px',color:'rgb(69, 131, 212)',fontWeight:'bold', border:'none'}}
+        />
+
+        <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal}>
+          <div className='Subbox'>
+            <img className='icon' src={userIcon} alt='User' />
+            <label className='Blabel' htmlFor='name' style={{ padding: '10px', fontSize: '18px' }}>Mac ID:</label>
+            <input type="text" id="nameBox" onChange={this.handleChange} style={{ fontSize: '16px' }} />
+          </div>
+          <div className='Subbox'>
+            <label className='Blabel' htmlFor='email' style={{ padding: '10px', fontSize: '18px' }}>Passwpord:</label>
+            <input type="password" id="pswBox" onChange={this.handleChange2} style={{ fontSize: '16px' }} />
+          </div>
+          <button id='CloseBtn'onClick={this.closeModal} style={{borderRadius: '12px'}}>Close</button>
+          <button id='SubmitBtn'onClick={this.submitModal} style={{borderRadius: '12px'}}>Submit</button>
+        </Modal>
+
+      </div>
+    );
+  }
+}
+export default Login;
--- front/src/Navigation.js
+++ front/src/Navigation.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 import { Nav, Navbar, Form, FormControl, NavDropdown } from 'react-bootstrap';
 import styled from 'styled-components';
+import Login from './Login';
+
 const Styles = styled.div`
   .navbar { background-color: #7A003C; }
   .nav-link {
@@ -38,44 +41,50 @@ const Styles = styled.div`
   }
 `;
 
-function clickSignOut(){
-  alert("This function is not availible in this test yet.")
+function clickSignOut() {
+  alert("You have been signed out!")
+  window.location.reload();
+  return (<Redirect to={`/`} />)
 }
 
 const NavigationBar = () => (
+  
   <Styles>
-    <Navbar expand="md" style={{textAlign:"right"}}>
-      <Navbar.Brand href="/" style={{color:"green", fontSize:"40px", fontWeight:'bold'}}>
-      <img
-        id='logo'
-        alt="QuickLib"
-        className="Logo"
-        src={require('./img/LogoNew.png')}
-        className="d-inline-block align-top"
-        width="300px"
-        height="80px"
-      />
-      
-    </Navbar.Brand>
+    <Navbar expand="md" style={{ textAlign: "right" }}>
+      <Navbar.Brand href="/" style={{ color: "green", fontSize: "40px", fontWeight: 'bold' }}>
+        <img
+          id='logo'
+          alt="QuickLib"
+          className="Logo"
+          src={require('./img/LogoNew.png')}
+          className="d-inline-block align-top"
+          width="300px"
+          height="80px"
+        />
+
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto" >
-        <Nav.Item><Nav.Link href="/" >Main</Nav.Link></Nav.Item>
-        <Nav.Item><Nav.Link href="/BrowsePage" >Browse</Nav.Link></Nav.Item>
-        <Nav.Item><Nav.Link href="/SettingPage" >Setting</Nav.Link></Nav.Item>
-        <Nav.Item><Nav.Link href="/SupportPage" >Support</Nav.Link></Nav.Item>
-        <NavDropdown title="Test User" id="basic-nav-dropdown" >
-            <NavDropdown.Item className='itemImg'><img width={'30px'} height={'30px'} src={"https://avatars2.githubusercontent.com/u/32040029?s=400&v=4"}></img></NavDropdown.Item>
-            <NavDropdown.Item className='itemU' style={{color:"black"}}>user@test.com</NavDropdown.Item>
+          <Nav.Item><Nav.Link href="/" >Main</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="/BrowsePage" >Browse</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="/SettingPage" >Setting</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href="/SupportPage" >Support</Nav.Link></Nav.Item>
+          
+          <NavDropdown title="Test User" id="basic-nav-dropdown" >
+            <NavDropdown.Item className='itemU' style={{ color: "black" }}>
+              <Login />
+            </NavDropdown.Item>
             <Nav.Item className='item' >
-            <Nav.Link className='item' href="/HistoryPage" style={{color:"black"}}>Booking History</Nav.Link></Nav.Item>
-            <NavDropdown.Divider/>
-            <NavDropdown.Item className='item' onClick={clickSignOut} style={{color:"black"}}>Sign Out</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
+              <Nav.Link className='item' href="/HistoryPage" style={{ color: "black", paddingTop:'0px' }}>Booking History</Nav.Link>
+            </Nav.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item className='item' onClick={clickSignOut} style={{ color: "black" }}>Sign Out</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
       </Navbar.Collapse>
       <Form className="form-center" >
-        <FormControl type="text" placeholder="Type here" className="" onChange={clickSignOut} value=""/>
+        <FormControl type="text" placeholder="Type here" className="" onChange={clickSignOut} value="" />
       </Form>
     </Navbar>
   </Styles>
